ee52dfbf046c01970c439a0c6b78284eb13a0c99
antoinetd <antoinetdelorme@hotmail.com>
2019-01-24 21:42:59 -0500

    First commit of blank project

---
+++ Assets/Scripts/AudioMgr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioMgr : MonoBehaviour
+{
+    public AudioClip[] soundEfx;
+    public AudioClip[] music; 
+    public GameObject _audioManager;
+    AudioSource _audioSource;   
+
+    // Use this for initialization
+    void Awake()
+    {
+        _audioSource = this.GetComponent<AudioSource>();
+
+        // Singleton
+        DontDestroyOnLoad(this.gameObject);
+    } 
+    
+}
+
+
---
+++ Assets/Scripts/SceneMgr.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneMgr : MonoBehaviour
+{
+    public void doChangeScene(int someScene)
+    {
+        switch (someScene)
+        {
+            case 0:
+                SceneManager.LoadScene(0);
+                break;
+
+            case 1:
+                SceneManager.LoadScene(1);
+                break;
+
+            case 2:
+                SceneManager.LoadScene(2);
+                break;
+        }
+
+    }
+}
