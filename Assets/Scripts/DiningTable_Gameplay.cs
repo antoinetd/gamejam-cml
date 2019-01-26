e1e7fb6638455e9304e520204cb39cb270d7df0f
phungluc <47018570+phungluc@users.noreply.github.com>
2019-01-26 14:12:47 -0500

    Add files via upload
    Creation of kitcen object script

---
+++ Assets/Scripts/DiningTable_Gameplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiningTable_Gameplay : MonoBehaviour, IInteractable
+{
+    // Plate Game Objects to make explode when the action is called
+    public GameObject Plate;
+    public bool ActionCalled; // Debug Output
+
+    // Private variable for the plate rigid body
+    private Rigidbody PlateRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the debug output
+        ActionCalled = false;
+
+        // Find the rigid body component of the plate
+        PlateRigidBody = Plate.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    // For debugging purposes
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            OnAction();
+        }
+    }
+
+    // Action to be called by the operator
+    // When the action is called, it will apply a force to the plate 
+    public void OnAction()
+    {
+        ActionCalled = true;
+
+        Vector3 Force = new Vector3(20.0f, 30.0f, 20.0f);
+        PlateRigidBody.AddForce(Force);
+    }
+}
