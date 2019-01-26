1f488aba7707764aa5319b474000f889899bded3
Thomas <thomas.friedlaender@gmail.com>
2019-01-26 14:34:57 -0500

    More work in progress on AI script: distance function, read from Antoine's closest objects

--- Assets/Scripts/AI_Ctrl.cs
+++ Assets/Scripts/AI_Ctrl.cs
@@ -4,8 +4,68 @@ using UnityEngine;
 
 public class AI_Ctrl : MonoBehaviour
 {
+    // Types
+    public enum AIStates
+    {
+        chasing,
+        blocking,
+        idle
+    };
+
+
+
+    // General functions
+    public float distanceBetweenObjects(GameObject Ob1, GameObject Ob2)
+    {
+        return (Ob1.GetComponent<Transform>().position - Ob2.GetComponent<Transform>().position).magnitude;
+    }
+
+
+
+    // Members
     public Transform someLocation;
-    public GameObject agent; 
+    public GameObject agent;
+    public GameObject kid;
+    public KidControls kidScript;
+    
+    
+    private AIStates stateValue = 0;
+
+
+    //Utility functions
+    public void setState(int someState)
+    {
+        switch(someState)
+        {
+            case 0:
+                stateValue = AIStates.blocking; 
+                break;
+            case 1:
+                stateValue = AIStates.chasing;
+                break;
+            case 2:
+                stateValue = AIStates.chasing;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void doChasing()
+    {
+        agent.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = kid.gameObject.GetComponent<Transform>().position;
+    }
+
+    public void doBlocking()
+    {
+        agent.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = kid.gameObject.GetComponent<Transform>().position;
+    }
+
+    public void doIdle()
+    {
+        //
+    }
+
 
 
     // Start is called before the first frame update
@@ -18,6 +78,19 @@ public class AI_Ctrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.GetComponent<UnityEngine.AI.NavMeshAgent>().destination = someLocation.position; 
+       
+      switch(stateValue)
+        {
+            case AIStates.blocking:
+                doBlocking();
+                break;
+            case AIStates.chasing:
+                doChasing(); 
+                break;
+            case AIStates.idle:
+                doIdle();
+                break; 
+        }
+        //var something = kidScript.closestsInteractables;
     }
 }
