0f46ebcfcad552098a12033c4d0094563a20b8e1
phungluc <phungluc@hotmail.com>
2019-01-27 05:33:26 -0500

    Creation of interactive logics for sink and toilet. Materials to be corrected.

---
+++ Assets/Scripts/GenericWaterAction.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericWaterAction : MonoBehaviour, IInteractable
+{
+    // Public Variables
+    public bool ActionCalled; // Debug Output
+    public GameObject ParticleObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ActionCalled = false;
+
+        // Set the particles to be initially off
+        ParticleObject.SetActive(false);
+
+
+    }
+
+    public void OnAction()
+    {
+
+        // Action to be called by the operator
+        ActionCalled = true;
+
+        // Trigger the particle emitter
+        ParticleObject.SetActive(true);
+
+    }
+
+}
